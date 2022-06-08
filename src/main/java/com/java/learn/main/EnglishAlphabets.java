60ac98c18aa7de0bd2a621b3fda990aa9491ed4c
yulia.lukianenko <ylukianenko@conceptsoft.io>
2022-06-08 23:44:18 +0300

    Init

---
+++ src/main/java/com/java/learn/main/EnglishAlphabets.java
@@ -0,0 +1,7 @@
+package com.java.learn.main;
+
+public class EnglishAlphabets {
+        public static void main(String[] args) {
+            System.out.println("Here will be english alphabet" + "a,b,c, d...");
+        }
+    }
