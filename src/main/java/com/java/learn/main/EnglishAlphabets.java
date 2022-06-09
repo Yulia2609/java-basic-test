aefbb86d9ffd4ac039e1184432bad54c52d91699
yulia.lukianenko <ylukianenko@conceptsoft.io>
2022-06-09 10:11:17 +0300

    Added git ignore list

--- src/main/java/com/java/learn/main/EnglishAlphabets.java
+++ src/main/java/com/java/learn/main/EnglishAlphabets.java
@@ -2,6 +2,6 @@ package com.java.learn.main;
 
 public class EnglishAlphabets {
         public static void main(String[] args) {
-            System.out.println("Here will be english alphabet" + "a,b,c, d...");
+            System.out.println("Here will be english alphabet" + "a,b,c,d...");
         }
     }
